de13f3a9907f62d86276940507dad0e690812528
Lauro Cabral <Lauro199471@gmail.com>
2019-03-25 21:19:29 -0700

    Finished Midterm 2 study guide questions

---
+++ midterm2_LauroCabral/midterm2_LauroCabral.ip_user_files/bd/system/system_stub.vhdl
@@ -0,0 +1,51 @@
+-- Copyright 1986-2018 Xilinx, Inc. All Rights Reserved.
+-- --------------------------------------------------------------------------------
+-- Tool Version: Vivado v.2018.3 (win64) Build 2405991 Thu Dec  6 23:38:27 MST 2018
+-- Date        : Mon Mar 25 20:34:56 2019
+-- Host        : Lauro-Laptop running 64-bit major release  (build 9200)
+-- Command     : write_vhdl -force -mode synth_stub
+--               C:/midterm2_LauroCabral/midterm2_LauroCabral.srcs/sources_1/bd/system/system_stub.vhdl
+-- Design      : system
+-- Purpose     : Stub declaration of top-level module interface
+-- Device      : xc7z010clg400-1
+-- --------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity system is
+  Port ( 
+    DDR_addr : inout STD_LOGIC_VECTOR ( 14 downto 0 );
+    DDR_ba : inout STD_LOGIC_VECTOR ( 2 downto 0 );
+    DDR_cas_n : inout STD_LOGIC;
+    DDR_ck_n : inout STD_LOGIC;
+    DDR_ck_p : inout STD_LOGIC;
+    DDR_cke : inout STD_LOGIC;
+    DDR_cs_n : inout STD_LOGIC;
+    DDR_dm : inout STD_LOGIC_VECTOR ( 3 downto 0 );
+    DDR_dq : inout STD_LOGIC_VECTOR ( 31 downto 0 );
+    DDR_dqs_n : inout STD_LOGIC_VECTOR ( 3 downto 0 );
+    DDR_dqs_p : inout STD_LOGIC_VECTOR ( 3 downto 0 );
+    DDR_odt : inout STD_LOGIC;
+    DDR_ras_n : inout STD_LOGIC;
+    DDR_reset_n : inout STD_LOGIC;
+    DDR_we_n : inout STD_LOGIC;
+    FIXED_IO_ddr_vrn : inout STD_LOGIC;
+    FIXED_IO_ddr_vrp : inout STD_LOGIC;
+    FIXED_IO_mio : inout STD_LOGIC_VECTOR ( 53 downto 0 );
+    FIXED_IO_ps_clk : inout STD_LOGIC;
+    FIXED_IO_ps_porb : inout STD_LOGIC;
+    FIXED_IO_ps_srstb : inout STD_LOGIC;
+    LED : out STD_LOGIC_VECTOR ( 3 downto 0 );
+    buttons_tri_i : in STD_LOGIC_VECTOR ( 3 downto 0 );
+    switches_tri_i : in STD_LOGIC_VECTOR ( 3 downto 0 )
+  );
+
+end system;
+
+architecture stub of system is
+attribute syn_black_box : boolean;
+attribute black_box_pad_pin : string;
+attribute syn_black_box of stub : architecture is true;
+attribute black_box_pad_pin of stub : architecture is "DDR_addr[14:0],DDR_ba[2:0],DDR_cas_n,DDR_ck_n,DDR_ck_p,DDR_cke,DDR_cs_n,DDR_dm[3:0],DDR_dq[31:0],DDR_dqs_n[3:0],DDR_dqs_p[3:0],DDR_odt,DDR_ras_n,DDR_reset_n,DDR_we_n,FIXED_IO_ddr_vrn,FIXED_IO_ddr_vrp,FIXED_IO_mio[53:0],FIXED_IO_ps_clk,FIXED_IO_ps_porb,FIXED_IO_ps_srstb,LED[3:0],buttons_tri_i[3:0],switches_tri_i[3:0]";
+begin
+end;
---
+++ midterm2_LauroCabral/midterm2_LauroCabral.sdk/midterm2/Debug/src/main.d
@@ -0,0 +1,46 @@
+src/main.o src/main.o: ../src/main.c \
+ ../../midterm2_bsp/ps7_cortexa9_0/include/xparameters.h \
+ ../../midterm2_bsp/ps7_cortexa9_0/include/xparameters_ps.h \
+ ../../midterm2_bsp/ps7_cortexa9_0/include/xgpio.h \
+ ../../midterm2_bsp/ps7_cortexa9_0/include/xil_types.h \
+ ../../midterm2_bsp/ps7_cortexa9_0/include/xil_assert.h \
+ ../../midterm2_bsp/ps7_cortexa9_0/include/xstatus.h \
+ ../../midterm2_bsp/ps7_cortexa9_0/include/xgpio_l.h \
+ ../../midterm2_bsp/ps7_cortexa9_0/include/xil_io.h \
+ ../../midterm2_bsp/ps7_cortexa9_0/include/xil_printf.h \
+ ../../midterm2_bsp/ps7_cortexa9_0/include/xparameters.h \
+ ../../midterm2_bsp/ps7_cortexa9_0/include/bspconfig.h \
+ ../../midterm2_bsp/ps7_cortexa9_0/include/xpseudo_asm.h \
+ ../../midterm2_bsp/ps7_cortexa9_0/include/xreg_cortexa9.h \
+ ../../midterm2_bsp/ps7_cortexa9_0/include/xpseudo_asm_gcc.h \
+ ../../midterm2_bsp/ps7_cortexa9_0/include/my_led_ip.h
+
+../../midterm2_bsp/ps7_cortexa9_0/include/xparameters.h:
+
+../../midterm2_bsp/ps7_cortexa9_0/include/xparameters_ps.h:
+
+../../midterm2_bsp/ps7_cortexa9_0/include/xgpio.h:
+
+../../midterm2_bsp/ps7_cortexa9_0/include/xil_types.h:
+
+../../midterm2_bsp/ps7_cortexa9_0/include/xil_assert.h:
+
+../../midterm2_bsp/ps7_cortexa9_0/include/xstatus.h:
+
+../../midterm2_bsp/ps7_cortexa9_0/include/xgpio_l.h:
+
+../../midterm2_bsp/ps7_cortexa9_0/include/xil_io.h:
+
+../../midterm2_bsp/ps7_cortexa9_0/include/xil_printf.h:
+
+../../midterm2_bsp/ps7_cortexa9_0/include/xparameters.h:
+
+../../midterm2_bsp/ps7_cortexa9_0/include/bspconfig.h:
+
+../../midterm2_bsp/ps7_cortexa9_0/include/xpseudo_asm.h:
+
+../../midterm2_bsp/ps7_cortexa9_0/include/xreg_cortexa9.h:
+
+../../midterm2_bsp/ps7_cortexa9_0/include/xpseudo_asm_gcc.h:
+
+../../midterm2_bsp/ps7_cortexa9_0/include/my_led_ip.h:
